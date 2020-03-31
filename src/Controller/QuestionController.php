36fc3c0c4ed2973fb372e7958eb46d53a587739b
afc.sagir <afc.sagir@gmail.com>
2020-03-31 12:40:41 +0600

    Controller

---
+++ src/Controller/QuestionController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Controller;
+use Symfony\Component\HttpFoundation\Response;
+
+class QuestionController{
+    public function homepage(){
+        return new Response('What a same');
+
+    }
+}
